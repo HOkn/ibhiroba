e8f14afdaba86f0232bf8c996fcf29120f916584
Haru Okaniwa <haruokny@gmail.com>
2015-04-04 03:28:11 +0900

    layout3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,26 +4,23 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-
   def show
     @user = current_user
-    @question = Question.find(params[:id])
+    @question = Question.find(params[:question_id])
     @question = Question.includes(:answers).find(params[:id])
     @answer = Answer.new
   end
-
 
   def new
     @question = Question.new
     @categories = Category.all
   end
 
-
   def create
     @user=current_user
     @question = Question.new(question_params)
     @category = Category.find(params[:category][:id])
-
+    @question.user_id = @user.id
     if @question.save & @question.categories << @category
       redirect_to user_question_path(@user, @question)
     else
@@ -31,37 +28,34 @@ class QuestionsController < ApplicationController
     end
   end
 
-
   def edit
     @user = current_user
     @question = Question.find(params[:id])
+    @categories = @question.categories
   end
 
-
   def update
+    raise
     @user = current_user
     @question = Question.find(params[:id])
+    @categories = Category.find(params[:category][:id])
     if @question.update_attributes(question_params)
-      @question.save
       redirect_to user_question_path(@user, @question)
     else
       render 'edit'
     end
   end
 
+  def index_category
+    @questions = Question.all
+    @question_id = QuestionCategory.question_id
 
-  # def index_category
-  #   @questions = Question.all
-  #   @questioncategory = QuestionCategory.all
-  #   @question_id = @questioncategory.question_id
-
-  #   @question = Question.where(@question_id: 1)
-  # end
-
+    # @question = Question.where(@question_id: 1)
+  end
 
   private
   def question_params
-    params.require(:question).permit(:title, :content, categories_attributes: [:id])
+    params.require(:question).permit(:title, :content, :user_id, categories_attributes: [:id])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :require_login
-  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def favorites
     @user = User.find(params[:id])
   end
